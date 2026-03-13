917903a948784186112f417aee5a6252ecfec78c
wearedood <117272307+wearedood@users.noreply.github.com>
2026-03-13 10:09:04 +0100

    Fix React capitals properly

--- index.tsx
+++ index.tsx
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// Polyfill for global
-if (typeof (window as any).global === 'undefined') {
-  (window as any).global = window;
-}
-
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
@@ -14,7 +9,7 @@ if (!rootElement) {
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
-  <react.strictmode>
-    <app/>
+  <React.StrictMode>
+    <App />
   </React.StrictMode>
 );
