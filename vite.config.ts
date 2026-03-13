9458d168d707f7f0b0d8578c1a0696019f5fbfb2
wearedood <117272307+wearedood@users.noreply.github.com>
2026-03-13 16:58:04 +0100

    Fix deployment imports and terminal hang

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,9 @@ import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    nodePolyfills()
-  ],
+  plugins: [react(), nodePolyfills()],
   build: {
-    // LE FIX ABSOLU : On interdit la compression qui casse tout
     minify: false,
-    // On s'assure que le code moderne (BigInt, etc.) est supporté par Stacks
     target: 'esnext'
   }
 })
