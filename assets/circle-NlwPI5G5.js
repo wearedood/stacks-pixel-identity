790be928ae07fa64fea578c03ea36b672759a6c0
wearedood <wearedood@users.noreply.github.com>
2026-03-18 17:26:14 +0000

    deploy: 3ba3c638ff09db5c202b3cd26f2bd7e9a8637c3c

--- assets/circle-NlwPI5G5.js
+++
@@ -1,9 +0,0 @@
-import { w } from './index-DKKC4k3P.js';
-import './index-CoEv_f-E.js';
-
-const circleSvg = w `<svg width="4" height="4" viewBox="0 0 4 4" fill="none" xmlns="http://www.w3.org/2000/svg">
-<circle cx="2" cy="2" r="2" fill="white" fill-opacity="0.2"/>
-</svg>
-`;
-
-export { circleSvg };
