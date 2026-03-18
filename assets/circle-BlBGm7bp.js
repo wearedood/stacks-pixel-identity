6e5b777ea3c5ca8d66cb8abc39d52a90ea18b46a
wearedood <wearedood@users.noreply.github.com>
2026-03-18 20:41:50 +0000

    deploy: 4f1b306c9cb8e4269f24f7686362d87d1d524632

---
+++ assets/circle-BlBGm7bp.js
@@ -0,0 +1,9 @@
+import { w } from './index-CHgFH3zP.js';
+import './index-CI5Vad8Y.js';
+
+const circleSvg = w `<svg width="4" height="4" viewBox="0 0 4 4" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="2" cy="2" r="2" fill="white" fill-opacity="0.2"/>
+</svg>
+`;
+
+export { circleSvg };
