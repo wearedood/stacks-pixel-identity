761ed7bb429d9cc08a3194d73df1985ae58b01ef
wearedood <117272307+wearedood@users.noreply.github.com>
2026-03-18 16:58:11 +0100

    Align GitHub with AI Studio version

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,11 @@ import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
+  base: '/stacks-pixel-identity/',
   plugins: [
     react(),
     nodePolyfills({
-      globals: {
-        Buffer: true,
-        global: true,
-        process: true,
-      },
+      globals: { Buffer: true, global: true, process: true },
     }),
   ],
   build: {
