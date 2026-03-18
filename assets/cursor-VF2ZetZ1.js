6faa7258496de1983a1a6038ad8fbb393b762fe5
wearedood <wearedood@users.noreply.github.com>
2026-03-18 17:48:14 +0000

    deploy: 5b9ff394f3f7922caa39680b820d6fcba2bcce68

---
+++ assets/cursor-VF2ZetZ1.js
@@ -0,0 +1,8 @@
+import { w } from './index-BC6Z63SD.js';
+import './index-DP2ZLVJ_.js';
+
+const cursorSvg = w ` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;
+
+export { cursorSvg };
