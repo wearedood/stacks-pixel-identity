1ae2c4f2e0f91d9394c66636996260b9133d5d64
wearedood <117272307+wearedood@users.noreply.github.com>
2026-03-18 17:01:36 +0100

    Updates

--- assets/circle-Zm0EiD4D.js
+++
@@ -1,9 +0,0 @@
-import { w } from './index-A6_7FS74.js';
-import './index-mhNXtrQ9.js';
-
-const circleSvg = w `<svg width="4" height="4" viewBox="0 0 4 4" fill="none" xmlns="http://www.w3.org/2000/svg">
-<circle cx="2" cy="2" r="2" fill="white" fill-opacity="0.2"/>
-</svg>
-`;
-
-export { circleSvg };
