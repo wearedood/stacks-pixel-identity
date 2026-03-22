f27ed7818dbd83d0555fd5a13060f3efb4ae0c7f
wearedood <117272307+wearedood@users.noreply.github.com>
2026-03-22 13:53:38 +0100

    fix: base path for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
-  base: '/stacks-pixel-identity/',
-  plugins: [
-    react(),
-    nodePolyfills(),
-  ],
-  define: {
-    global: 'window',
-  },
-  build: {
-    minify: false,
-    target: 'esnext'
-  }
+  plugins: [react()],
+  base: '/',
 })
